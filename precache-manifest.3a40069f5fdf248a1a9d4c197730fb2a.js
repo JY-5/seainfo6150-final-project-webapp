7b8a81c58db06832c25bc0bd5c033dfc234fdbcd
JY-5 <58899117+JY-5@users.noreply.github.com>
2020-12-10 04:09:33 -0800

    Updates

---
+++ precache-manifest.3a40069f5fdf248a1a9d4c197730fb2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bf494dcdfec615a0253116259b51fe",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "01209003b391ef291cf8",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.fc97496a.chunk.css"
+  },
+  {
+    "revision": "93669c78f2d326465002",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.5d0c3283.chunk.js"
+  },
+  {
+    "revision": "01209003b391ef291cf8",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.a409ba20.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "29bb5a3bebee3ed93e62d6fd624335a1",
+    "url": "/seainfo6150-final-project-webapp/static/media/roastedChicken1.29bb5a3b.jpg"
+  },
+  {
+    "revision": "c4dd426eb2f241819b7860448689eb44",
+    "url": "/seainfo6150-final-project-webapp/static/media/roastedChicken2.c4dd426e.jpg"
+  },
+  {
+    "revision": "f222a730fbc5776c60b472458a3e51a6",
+    "url": "/seainfo6150-final-project-webapp/static/media/roastedChicken3.f222a730.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seainfo6150-final-project-webapp/precache-manifest.247f04989548970dcbda5769a0f254ed.js"
+  "/seainfo6150-final-project-webapp/precache-manifest.3a40069f5fdf248a1a9d4c197730fb2a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/seainfo6150-final-project-webapp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
